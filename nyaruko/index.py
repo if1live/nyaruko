579f2a3dbc96dd009efcab7e2b39e30e16f5144d
omniavinco <omniavinco@gyuseon-ui-MacBook-Pro.local>
2012-07-28 20:37:38 +0900

    html_minify : _m function

--- nyaruko/index.py
+++ nyaruko/index.py
@@ -1,18 +1,22 @@
 import os
 from flask import Flask
 from flask import render_template
+from htmlmin.minify import html_minify
 
 app = Flask(__name__)
 
+_m = html_minify
+
 app.add_url_rule('/favicon.ico',
                  redirect_to=url_for('static', filename='favicon.ico'))
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return _m(render_template('index.html'))
 
 @app.route('/search')
 def search():
-    return render_template('main.html')
+    return _m(render_template('main.html'))
 
 if __name__ == '__main__':
-               app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0')
